bedc7890855674f72cfe41de2aacd0a964c7d49e
Anibal Copitan <anibal@pprios.com>
2021-05-02 21:28:51 -0500

    [add] serverless & aws

---
+++ src/services/PeopleService.js
@@ -0,0 +1,59 @@
+const {
+  successResponse,
+  errorResponse,
+  customResponse,
+} = require("../utils/lambda-response");
+const PeopleModel = require("../models/People");
+const axios = require("axios");
+const peopleModel = new PeopleModel();
+const swapiURL = process.env.SWAPI_URL;
+
+/*
+ * Service People   
+*/
+class PeopleService {
+
+  static async create(request) {
+    try {
+
+      // await peopleModel.create(request.data);
+
+      let dataJSON = JSON.parse(request.body);
+      let rs = await peopleModel.create(dataJSON);
+      console.log('rs', rs);
+
+      return successResponse({
+        message: "Exito",
+      });
+    } catch (e) {
+      console.log("error", e);
+      
+      return errorResponse({ message: "Error Interno" });
+    }
+  }
+
+  static async getAll() {
+    try {
+      const data = await peopleModel.getAll();
+
+      if (Object.keys(data).length === 0) {
+        return customResponse(
+          {
+            message: "Empty",
+          },
+          404
+        );
+      }
+
+      return successResponse({
+        message: "Exito",
+        data: data.Items,
+      });
+    } catch (e) {
+      console.log("error", e);
+      return errorResponse({ message: "Error Interno", data: null });
+    }
+  }
+}
+
+module.exports = PeopleService;
